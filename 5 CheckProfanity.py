c1e9c882a990caadaa512ac23691ef58521d847b
Harshil Darji <darjiharshil2994@gmail.com>
2016-07-28 12:04:46 +0530

    Improvement

--- 5 CheckProfanity.py
+++ 5 CheckProfanity.py
@@ -10,16 +10,22 @@ def readText(path):
     for item in text:
         checkProfanity(item, line)
         line += 1
-        
+
+profanityResult = []
 def checkProfanity(text, line):
     address = ("http://www.wdylike.appspot.com/?q=%s" % text).replace(' ', '%20')
     connection = url.urlopen(address)
     output = str(connection.read())[1:]
     if output == "'true'":
-        print('Line #%s: %s | %s' % (line, output.title(), text))
+        profanityResult.append('Line #%s: %s | %s' % (line, output.title(), text))
     connection.close()
 
 cwd = os.getcwd()
 path = cwd + '\MovieQuotes.txt'
 print('Checking profanity...')
 readText(path)
+if not profanityResult:
+    print('No profanity found!')
+else:
+    for item in profanityResult:
+        print(item)
